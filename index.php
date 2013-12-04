f875fa79e8add34099ed3745b10bb6f9b55c4077
Luiz Picolo <luizpicolo@gmail.com>
2013-12-04 14:23:30 -0300

    Modificada extensão para Deploy no Heroku

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js"><!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+        <title>Sistema CODE - Controle de Ocorrências e Desempenho Escolar</title>
+        
+        <meta name="description" content="O CODE é um software em desenvolvimento com o objetivo de controlar parte do processo de ocorrências e relatórios de desempenho escolar de forma prática, rápida e segura.">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="shortcut icon" href="img/code-icon.png">
+        <link rel="stylesheet" href="css/principal.css">
+        <script src="js/tema/modernizr-2.6.2.min.js"></script>
+    </head>
+
+    <body>
+        <div id="cabecalho">
+            <div id="cabecalho-recipiente">
+                <div class="grade">
+                    <header>
+                        <h1 id="logo">C.O.D.E</h1>
+                    </header>
+                    
+                    <article id="resumo">
+                        <h2>Controle de Ocorrências e Desempenho Escolar</h2>
+                        <p>É um software em desenvolvimento com o objetivo 
+                        de controlar parte do processo de ocorrências e relatórios de 
+                        desempenho escolar de forma prática, rápida e segura.</p>
+                    </article>
+                    
+                    <div id="lancamento">
+                        <p>Seja o primeiro a saber quando lançamos!</p>
+                        
+                        <form action="http://sistemacode.us3.list-manage.com/subscribe/post?u=728af9f3c6ce33a5674ec0156&amp;id=299031361f" method="post">
+                            <label for="email">Digite seu e-mail</label>
+                            <input type="email" name="EMAIL" id="email" placeholder="Digite seu e-mail" required>
+                            <button>Cadastrar <i class="fa fa-rocket"></i></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div id="principal">
+            <div id="principal-recipiente">
+                <main>
+                    <div class="grade">
+                        <article id="sobre-o-projeto">
+                            <h2>Conheça mais sobre o projeto</h2>
+                            
+                            <section>
+                                <h3><i class="fa fa-lightbulb-o"></i> O que é?</h3>
+                                <p>O <strong>C.O.D.E</strong> é um sistema idealizado para  automatizar o processo de cadastro, listagem e geração de relatórios das ocorrências e desempenhos escolares de forma simplificada.</p>
+                            </section>
+                            
+                            <section>
+                                <h3><i class="fa fa-crosshairs"></i> Objetivo</h3>
+                                <p>Nosso maior objetivo é proporcionar aos nossos futuros clientes um sistema que controle todo o processo de ocorrências e relatórios de desempenho escolar de forma prática e segura. Neste sentido, estamos trabalhando em um sistema limpo é agil, focado no usuário e em suas necessidades.</p>
+                            </section>
+                            
+                            <section>
+                                <h3><i class="fa fa-users"></i> Público alvo</h3>
+                                <p>Nosso público alvo são as instituições de ensino em seus diversos níveis, desde a educação básica ao ensino médico. Dentre estas diversas etápas, buscamos as pequenas e medias instituições que necessitam de um sistema com baixo custo e alto benefício.
+</p>
+                            </section>
+                        </article>
+                    </div>
+                </main>
+                        
+                <footer id="footer">
+                    <address>contato@sistemacode.com.br</address>
+                </footer>
+            </div>
+        </div>
+                        
+        <!--<script src="js/tema/jquery-1.10.2.min.js"></script>-->
+        <!--<script src="js/tema/plugins.js"></script>-->
+        <script src="js/principal.js"></script>
+    </body>
+</html>
